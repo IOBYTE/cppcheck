0cb283237aeedf577dab07c0989bdf456a423e3b
Robert Reif <reif@earthlink.net>
2026-08-06 19:59:54 -0400

    rename test

--- test/cli/vcxproj_forced_includes_test.py
+++ test/cli/vcxproj_forced_includes_test.py
@@ -1,27 +1,27 @@
 
-# python -m pytest forced1_test.py
+# python -m pytest vcxproj_forced_includes_test.py
 
 import os
 
 from testutils import cppcheck
 
 __script_dir = os.path.dirname(os.path.abspath(__file__))
-__proj_dir = os.path.join(__script_dir, 'forced1')
+__proj_dir = os.path.join(__script_dir, 'vcxproj_forced_includes')
 
 def get_lines(s):
     return sorted(s.split('\n'))
 
-def test_forced1():
+def test_vcxproj_forced_includes():
     args = [
         '--template=cppcheck1',
-        '--project=forced1/forced1.cppcheck',
+        '--project=vcxproj_forced_includes/vcxproj_forced_includes.cppcheck',
         '--no-cppcheck-build-dir'
     ]
     ret, stdout, stderr = cppcheck(args, cwd=__script_dir)
-    filename1 = os.path.join('forced1', 'DebugX64.cpp')
-    filename2 = os.path.join('forced1', 'DebugX64.h')
-    filename3 = os.path.join('forced1', 'AllX64.h')
-    filename4 = os.path.join('forced1', 'GlobalDebugX64.h')
+    filename1 = os.path.join('vcxproj_forced_includes', 'DebugX64.cpp')
+    filename2 = os.path.join('vcxproj_forced_includes', 'DebugX64.h')
+    filename3 = os.path.join('vcxproj_forced_includes', 'AllX64.h')
+    filename4 = os.path.join('vcxproj_forced_includes', 'GlobalDebugX64.h')
     assert ret == 0, stdout
     expected = (
         '[%s:6]: (error) Division by zero.\n'
